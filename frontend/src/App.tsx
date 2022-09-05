895c397fe45b6406c4da0734e047028008c82737
Maxwell Barros <maxwellbarros@yahoo.com.br>
2022-09-05 16:59:15 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,15 @@
+import NotificationButton from "./components/NotificattionButton"
+
 function App() {
   return (
-    <h1>Olá mundo!</h1>
+    <>
+      <h1>Olá mundo!</h1>
+      <NotificationButton />
+      <NotificationButton />
+    </>
   )
 
-  
+
 }
 
 export default App
